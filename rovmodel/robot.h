195adfcfbd77b03b1e6772eed51d09c477bbd7a5
SystemChanger <Orischenko2007@yandex.ru>
2015-02-19 21:14:19 +0300

    Добавлен стабилизатор.
    Стабилизатор (глубины/мощности двигателя/погрешности)

--- rovmodel/robot.h
+++ rovmodel/robot.h
@@ -9,17 +9,28 @@ class Robot : public FloatingObject
     float maxEnginePower;
     float depthToHold;
     float proportionalFact;
+    float stabilizerValue;
+    float EngPowerDiff;
+    float oldEngPower;
+    float newEngPower;
 
 public:
     Robot(float mass, float archimedForce, float waterResistanceK, float enginePower, float maxEnginePower, float proportionalFact);
 
     void setEnginePower(float power);
+    void setOldEngPower(float oldPower);
+    void setNewEngPower(float newPower);
     void setDepthToHold(float depthToHold);
+    void setStabilizerValue(float stabilizerValue);
     void regulateDepth();
 
     float getEnginePower();
+    float getMaxEnginePower();
+    float getOldEngPower();
+    float getNewEngPower();
     float getDepthToHold();
     float getProportionalFact();
+    float getStabilizerValue();
 
     virtual float getForces();
     virtual void process();
